fe8e0091cb3c9866e49ba266da9c0839fae7b5ca
Yi-07 <1825901670@qq.com>
2026-05-21 16:49:41 +0800

    fix: pin unpkg CDN URLs to explicit version to avoid redirects
    unpkg @18 redirects to @18.3.1 in srcdoc iframes (origin: null),
    which may fail silently. Pinned react/react-dom to 18.3.1 explicitly.

--- components/artifact/ArtifactSandbox.tsx
+++ components/artifact/ArtifactSandbox.tsx
@@ -65,8 +65,8 @@ function buildSrcdoc(type: ArtifactType, code: string): string {
 <head>
 <meta charset="utf-8" />
 ${SENDPROMPT_SCRIPT}
-<script src="https://unpkg.com/react@18/umd/react.development.min.js" ><\/script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.min.js" ><\/script>
+<script src="https://unpkg.com/react@18.3.1/umd/react.development.min.js" ><\/script>
+<script src="https://unpkg.com/react-dom@18.3.1/umd/react-dom.development.min.js" ><\/script>
 <script src="https://unpkg.com/@babel/standalone/babel.min.js" ><\/script>
 <style>
   * { margin: 0; padding: 0; box-sizing: border-box; }
